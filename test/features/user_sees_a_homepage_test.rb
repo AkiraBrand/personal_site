b5ee72b49726e4918ef3de22676084432657cd81
Ruth Brand <ruthbrandsoprano@gmail.com>
2018-09-27 14:15:41 -0600

    working on getting error message test to pass

--- test/features/user_sees_a_homepage_test.rb
+++ test/features/user_sees_a_homepage_test.rb
@@ -3,8 +3,21 @@ require './test/test_helper'
 class HomepageTest < CapybaraTestCase
   def test_user_can_see_the_homepage
     visit '/'
-    save_and_open_page
+
     assert page.has_content?("Welcome!")
     assert_equal 200, page.status_code
   end
+  #
+  # Try to see if you can write a test before you implement the code below.
+  # It should attempt to visit some page that does not exist,
+  # assert that we get a 404 status code (indicating that the client has made an error),
+  # and assert that the page has some sort of message indicating
+  # that the page doesn't exist.
+
+  def test_user_sees_an_error_message_when_visiting_nonexistent_page
+    visit 'hello'
+    assert page.has_content?("page not found")
+    assert_equal 404, page.status_code
+  end
+
 end
